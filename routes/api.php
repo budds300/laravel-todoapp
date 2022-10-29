87509e7fb593d6824407eba7346117f200747131
Lawrence Macharia <macharialawrence2011@gmail.com>
2022-10-29 07:11:09 +0300

    formatted the "/user" response and added auth:web middleware on the "/auth/logout" route.

--- routes/api.php
+++ routes/api.php
@@ -7,6 +7,7 @@ use App\Models\Task;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -19,10 +20,12 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+    // return $request->user();
+    return ['status' => 'success', 'message' => 'User details', 'data' => $request->user()];
 });
 
 Route::post('auth/register', [AuthController::class, 'register']);
 Route::post('auth/login', [AuthController::class, 'login']);
+Route::post('auth/logout', [AuthController::class, 'logout'])->middleware('auth:web'); // This tells the logout API call to use SessionGuard instead of RequestGuard
 
 Route::resource('tasks', TaskController::class);
