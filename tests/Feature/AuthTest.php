8f75cb6a3ca290c4f6e2d7cfffb429734b7fde26
Lawrence Macharia <macharialawrence2011@gmail.com>
2022-10-29 07:15:21 +0300

    new tests function added and modifications

--- tests/Feature/AuthTest.php
+++ tests/Feature/AuthTest.php
@@ -2,51 +2,119 @@
 
 namespace Tests\Feature;
 
-use Illuminate\Foundation\Testing\RefreshDatabase;
+use App\Models\User;
+use Faker\Factory;
 use Illuminate\Foundation\Testing\WithFaker;
+use Laravel\Sanctum\Sanctum;
 use Tests\TestCase;
 
 class AuthTest extends TestCase
 {
     use WithFaker;
-    /**
-     * A basic feature test example.
-     *
-     * @return void
-     */
+
     public function test_register()
     {
-        // $password = $this->faker->password();
+        // user details
         $password = '123456';
         $user = ['name' => $this->faker->name(), 'email' => $this->faker->email(), 'password' => $password, 'password_confirmation' => $password];
 
+        // post
         $response = $this->postJson('api/auth/register', $user);
+
+        // log
         $response->dump();
 
+        // assert
         $response->assertStatus(200)->assertJsonStructure(['status', 'message', 'data']);
     }
 
-    public function test_login_error(){
-        $user = ['email' => 'hpurdy@gmail.com', 'password' => '1234566'];
+    public function test_register_validation_error()
+    {
+        // user details
+        $password = '123456';
+        $user = ['name' => $this->faker->name(), 'email' => $this->faker->email(), 'password' => $password, 'password_confirmation' => ''];
 
-        $response = $this->postJson('api/auth/login', $user);
+        // post
+        $response = $this->postJson('api/auth/register', $user);
+
+        // log
         $response->dump();
 
+        // assert
+        $response->assertStatus(422);
+        $response->assertJsonValidationErrors('password');
+        $this->assertGuest();
+    }
+
+    public function test_login_error()
+    {
+        // user details
+        $user = ['email' => 'hpurdy@gmail.com', 'password' => '1234566'];
+
+        // post
+        $response = $this->postJson('api/auth/login', $user);
+
+        // log
+        $response->dump();
+
+        // assert
         $response->assertStatus(401)->assertJsonStructure(['status', 'message', 'data']);
     }
 
-    public function test_login_success(){
-        $user = ['email' => 'hpurdy@gmail.com', 'password' => '123456'];
+    public function test_login_success()
+    {
+        // user details
+        $user = ['email' => 'bayer.jerald@hotmail.com', 'password' => '123456'];
 
+        // post
         $response = $this->postJson('api/auth/login', $user);
-        echo $response->getCookie('XSRF-TOKENM');
-        exit;
 
-        $response->assertSessionHas('login_web_59ba36addc2b2f9401580f014c7f58ea4e30989d');
-        // $response->assertCookie('XSRF-TOKEN');
+        // log
+        $response->dump();
+        $response->ddSession();
 
+        // assert
+        $response->assertStatus(200);
+    }
 
+    public function test_user()
+    {
+        // Authentication
+        $user = ['email' => 'bayer.jerald@hotmail.com', 'password' => '123456'];
+        $this->postJson('api/auth/login', $user);
+        Sanctum::actingAs(auth()->user());
 
-        // $response->assertStatus(200)->assertJsonStructure(['status', 'message', 'data']);
+        // get
+        $response = $this->getJson('/api/user');
+
+        // log
+        $response->dump();
+
+        // assert
+        $response->assertOk();
+    }
+
+    public function test_logout()
+    {
+        // Authentication
+        $user = ['email' => 'bayer.jerald@hotmail.com', 'password' => '123456'];
+        $this->postJson('api/auth/login', $user);
+        Sanctum::actingAs(auth()->user());
+
+        // // post - logout
+        $response = $this->postJson('api/auth/logout');
+
+        // // log
+        $response->dump();
+
+        // assert - check if token is still valid by trying to create a new task
+        if (!$this->assertGuest()) {
+            $task = ['text' => $this->faker->text(20), 'day' => $this->faker->date, 'reminder' => $this->faker->numberBetween(0, 1)];
+            $response = $this->postJson('api/tasks', $task);
+
+            $response->dump();
+
+            $response->assertStatus(200);
+        }
     }
 }
--- tests/Feature/TaskTest.php
+++ tests/Feature/TaskTest.php
@@ -2,41 +2,47 @@
 
 namespace Tests\Feature;
 
+use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
+use Laravel\Sanctum\Sanctum;
 use Tests\TestCase;
 
 class TaskTest extends TestCase
 {
     use WithFaker;
 
-    /**
-     * A basic feature test example.
-     *
-     * @return void
-     */
+
     public function test_index()
     {
         $response = $this->getJson('api/tasks');
-        // $response->dd();
+        $response->decodeResponseJson();
 
         $response->assertStatus(200);
     }
 
     public function test_store()
     {
-        $task = ['text' => $this->faker->text(20), 'day' => $this->faker->date, 'reminder' => $this->faker->numberBetween(0, 1)];
+        // Authentication
+        $user = ['email' => 'bayer.jerald@hotmail.com', 'password' => '123456'];
+        $this->postJson('api/auth/login', $user);
+        Sanctum::actingAs(auth()->user());
 
+        // post
+        $task = ['text' => $this->faker->text(20), 'day' => $this->faker->date, 'reminder' => $this->faker->numberBetween(0, 1)];
         $response = $this->postJson('api/tasks', $task);
+
+        // log
         // $response->dd();
 
-        $response->assertStatus(200);
+        // assert
+        $response->assertOk(200);
     }
 
     public function test_show()
     {
-        $response = $this->getJson('api/tasks/4');
-        // $response->dd();
+        $response = $this->getJson('api/tasks/3');
+        $response->dd();
 
         $response->assertStatus(200)
             ->assertJson(['message' => 'success']);
@@ -44,17 +50,35 @@ class TaskTest extends TestCase
 
     public function test_update()
     {
+        // Authentication
+        $user = ['email' => 'bayer.jerald@hotmail.com', 'password' => '123456'];
+        $this->postJson('api/auth/login', $user);
+        Sanctum::actingAs(auth()->user());
+
+        // put
         $response = $this->putJson('api/tasks/5', ['text' => $this->faker->text(20), 'day' => $this->faker->date, 'reminder' => $this->faker->numberBetween(0, 1)]);
+
+        // log
         // $response->dd();
 
+        //assert
         $response->assertStatus(200);
     }
 
     public function test_destroy()
     {
+        // Authentication
+        $user = ['email' => 'bayer.jerald@hotmail.com', 'password' => '123456'];
+        $this->postJson('api/auth/login', $user);
+        Sanctum::actingAs(auth()->user());
+
+        // delete
         $response = $this->deleteJson('api/tasks/3');
+
+        // log
         // $response->dd();
 
+        // assert
         $response->assertStatus(200);
     }
 }
